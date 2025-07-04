00c0528166b8034ce0ae88ce5dff5d7ea5b5fece
vasanthSECE <155640162+vasanthSECE@users.noreply.github.com>
2025-07-04 10:20:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ const displayMessage = msg => {
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  // No input
+ 
   if (!guess) {
-    displayMessage('⛔ No number!');
+    displayMessage(' No number!');
 
   // Correct guess
   } else if (guess === secretNumber) {
-    displayMessage('🎉 Correct Number!');
+    displayMessage('Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.body.style.backgroundColor = '#60b347';
 
@@ -24,20 +24,20 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore;
     }
 
-  // Wrong guess
+
   } else {
     if (score > 1) {
       displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      displayMessage('💥 You lost the game!');
+      displayMessage(' You lost the game!');
       document.querySelector('.score').textContent = 0;
     }
   }
 });
 
-// Reset
+
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
